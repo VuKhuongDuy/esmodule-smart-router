ce8e0f327b02223e2b48e73a600108c1babdbc25
Vũ Khương Duy <duy.vk@zinza.com.vn>
2024-01-03 17:09:42 +0000

    pancake

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,58 @@
-import { DydxSDK } from "./dydx/services/service.js";
-import {
-  DYDX_TOKENS,
-  PORT,
-} from "./constants/constants.js";
-import { checkForNewPosition, checkToClosePosition, getMarket, getMarkets } from "./service.js";
-import * as telegram from "./telegram/index.js";
-import { app, initServer } from "./apis/server.js";
-import { KwentaSDKCustom } from "./kwenta/services.js";
+import { SmartRouter } from '@pancakeswap/smart-router/evm'
+import { createPublicClient, http } from 'viem'
+import { bsc } from 'wagmi/chains'
+import { GraphQLClient } from 'graphql-request'
+import { Native, ChainId, CurrencyAmount, TradeType, Percent } from '@pancakeswap/sdk'
+import { bscTokens } from '@pancakeswap/tokens'
 
-export let kwentaSdk: KwentaSDKCustom;
-export let dydxClient: DydxSDK;
-let startScan = true;
-
-const init = async () => {
-  kwentaSdk = new KwentaSDKCustom();
-  dydxClient = new DydxSDK();
-};
+const publicClient = createPublicClient({
+  chain: bsc,
+  transport: http('https://bsc-dataseed1.binance.org'),
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+    },
+  },
+})
 
 const main = async () => {
-  await init();
+  const quoteProvider = SmartRouter.createQuoteProvider({
+    onChainProvider: () => publicClient,
+  })
+  const v3SubgraphClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-bsc')
+  const v2SubgraphClient = new GraphQLClient('https://proxy-worker-api.pancakeswap.com/bsc-exchange')
 
-  initServer(kwentaSdk, dydxClient);
+  const swapFrom = bscTokens.usdt
+  const swapTo = Native.onChain(56)
+  const amount = CurrencyAmount.fromRawAmount(swapFrom, 10 ** 16)
 
-  // setInterval(async () => {
-    if (startScan) {
-      const markets = await getMarkets(kwentaSdk, dydxClient);
-      const kwentaPositions = [] as any;
-      const dydxPositions = await dydxClient.getDydxPositions();
 
-      dydxPositions.forEach(async (pos) => {
-        const pair = pos.market;
-        const index = DYDX_TOKENS.findIndex((m) => m === pair);
-        
-        if (index >= 0) {
-          const marketDexes = await getMarket(dydxClient, markets, index);
-          const kPosition = await kwentaSdk.kwentaGetPositions(
-            marketDexes.kwenta
-          );
-          kwentaPositions.push(kPosition);
-          await checkToClosePosition(kwentaSdk, dydxClient, marketDexes, {
-            dydx: pos,
-            kwenta: kPosition[0],
-            gmx: null
-          });
-        }
-      });
+  const [v2Pools, v3Pools] = await Promise.all([
+    SmartRouter.getV2CandidatePools({
+      onChainProvider: () => publicClient,
+      v2SubgraphProvider: () => v2SubgraphClient,
+      v3SubgraphProvider: () => v3SubgraphClient,
+      currencyA: amount.currency,
+      currencyB: swapTo,
+    }),
+    SmartRouter.getV3CandidatePools({
+      onChainProvider: () => publicClient,
+      subgraphProvider: () => v3SubgraphClient,
+      currencyA: amount.currency,
+      currencyB: swapTo,
+      subgraphCacheFallback: false,
+    }),
+  ])
+  const pools = [...v2Pools, ...v3Pools]
+  const trade = await SmartRouter.getBestTrade(amount, swapTo, TradeType.EXACT_INPUT, {
+    gasPriceWei: () => publicClient.getGasPrice(),
+    maxHops: 2,
+    maxSplits: 2,
+    poolProvider: SmartRouter.createStaticPoolProvider(pools),
+    quoteProvider,
+    quoterOptimization: true,
+  })
 
-      await telegram.sendMessage(`----------START CHECK---------`);
-
-      for (let i = 0; i < DYDX_TOKENS.length; i++) {
-        console.log("----------------------", DYDX_TOKENS[i], "\t");
-        const marketDexes = await getMarket(dydxClient, markets, i);
-        await checkForNewPosition(
-          marketDexes,
-          kwentaSdk,
-          dydxClient,
-          DYDX_TOKENS[i]
-        );
-      }
-    }
-  // }, 2 * 60 * 1000);
 };
-
-app.get('/stop', async (req, res) => {
-  startScan = false
-  res.send('Stopped scan')
-})
-
-app.get('/start', async (req, res) => {
-  startScan = true
-  res.send('Started scan')
-})
-
-app.listen(PORT, function () {
-  console.log(`Server is listening on PORT ${PORT}`);
-});
 
 main();
